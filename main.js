980ab7d224fdba4b64bd28bb1f7b905d392156db
VedantGarg450 <86777859+VedantGarg450@users.noreply.github.com>
2022-01-19 19:42:06 +0530

    Add files via upload

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+ noseX= ""
+ noseY= ""
 function preload(){
 
 }
@@ -6,7 +8,26 @@ function setup(){
   canvas.center()
   video= createCapture(VIDEO)
   video.hide()
+  posenet= ml5.poseNet(video, Model_Loaded)
+  posenet.on("pose", gotPoses)
+}
+function gotPoses(result){
+  if(result.length>0){ 
+    console.log(result)
+    noseX= result[0].pose.nose.x
+    noseY= result[0].pose.nose.y
+    console.log(noseX)
+    console.log(noseY)
+  }
+}
+function Model_Loaded(){
+
+    console.log("Model Loaded Successfully!")
+  
 }
 function draw(){
     image(video, 00, 00, 600, 600)
+}
+function SNAPSHOT(){
+    save("unnamed.png")
 }
